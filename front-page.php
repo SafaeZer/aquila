ec6042b561e61f945f137b2c770fe61f0bac222c
safae zerrouki <safaezzerrouqi@gmail.com>
2022-03-25 12:10:19 +0100

    wp template hierarchy, wordpress posts page, grid post page, wordpress get_search_form, blog posts template

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Front page template
+ * 
+ * @package aquila
+ */
+get_header();
+?>
+<div>Front page</div>
+
+<?php get_footer();  ?>
---
+++ page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Page template.
+ *
+ * @package Aquila
+ */
+get_header();
+?>
+
+<div>Single page</div>
+
+<?php get_footer();  ?>
